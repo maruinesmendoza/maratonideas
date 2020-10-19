629941b8f538a4079c52a0119294cb1d530fd6b0
maruinesmendoza <53840283+maruinesmendoza@users.noreply.github.com>
2020-10-19 18:01:16 -0300

    Add files via upload

---
+++ index.categoria.php
@@ -0,0 +1,22 @@
+<?php
+
+class Categoria
+
+{
+    public $Jugadores;
+    public $coloreslibrito;
+
+
+    public function Jugadores() 
+    {
+        $juagadores= $this-> Jugadores;
+    }
+
+    public function Coloreslibrito()
+    {
+        $colreslibrito = array('0' => '6' );
+        return $this->coloreslibrito;
+    }
+
+}
+?>
---
+++ index.juego.php
@@ -0,0 +1,38 @@
+<?php
+
+Class Juego 
+{
+    public $preguntas;
+    public $respuestas; 
+
+      
+    
+  public  function Preguntas()
+    {
+    $preguntas['i'] = $this-> Preguntas[100];
+     {
+            for ($i = 0; $i < $preguntas; ++$i) 
+   
+        
+            return $this-> Preguntas;
+    }
+     }
+
+   
+     public function Respuestas()
+           {
+         return $this-> Respuestas;
+           }
+            
+        
+   
+
+
+
+}
+     
+
+
+
+
+?>
